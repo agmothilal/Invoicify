acbeb9a93180d04002ac93cb190b87a35d734b40
agmothilal <ag.mothilal@gmail.com>
2021-05-08 10:22:09 -0500

    GREEN: Item repo and service with fetch method.

--- src/main/java/com/groot/invoicify/service/ItemService.java
+++ src/main/java/com/groot/invoicify/service/ItemService.java
@@ -1,10 +1,12 @@
 package com.groot.invoicify.service;
 
 import com.groot.invoicify.dto.ItemDto;
+import com.groot.invoicify.entity.Item;
 import com.groot.invoicify.repository.ItemRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ItemService {
@@ -16,6 +18,14 @@ public class ItemService {
     }
 
     public List<ItemDto> fetchItems() {
-        return null;
+        var items = this.itemRepository.findAll();
+        return items.stream().map(ItemService::MapToDto).collect(Collectors.toList());
+    }
+
+    private static ItemDto MapToDto(Item item) {
+        return new ItemDto(item.getDescription(),
+                item.getRateHourBilled(),
+                item.getRatePrice(),
+                item.getFlatPrice());
     }
 }
