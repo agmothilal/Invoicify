2c03318e6fa90a40c97eedcd925526c029b58503
Christopher-Diehl-cognizant-com <cld71@cld-pc2.diehl>
2021-05-08 11:48:07 -0500

    Green:Get Item controller test

--- src/test/java/com/groot/invoicify/integration/ItemServiceTest.java
+++ src/test/java/com/groot/invoicify/integration/ItemServiceTest.java
@@ -1,5 +1,6 @@
 package com.groot.invoicify.integration;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.groot.invoicify.dto.ItemDto;
 import org.junit.jupiter.api.Test;
@@ -10,12 +11,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.servlet.function.ServerResponse;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -37,6 +40,31 @@ public class ItemServiceTest {
 				.content(this.objectMapper.writeValueAsString(itemDto)))
 				.andExpect(MockMvcResultMatchers.status().isCreated())
 				.andDo(MockMvcRestDocumentation.document("Post-Item", PayloadDocumentation.requestFields(
+						PayloadDocumentation.fieldWithPath("description").description("Item description"),
+						PayloadDocumentation.fieldWithPath("rateHourBilled").description("Item quantity"),
+						PayloadDocumentation.fieldWithPath("ratePrice").description("Item rate price"),
+						PayloadDocumentation.fieldWithPath("flatPrice").description("Item flat price")
+				)));
+	}
+
+	@Test
+	public void getItemTest() throws Exception {
+		var itemDto = new ItemDto("description", 1, 1.1f, 1.1f);
+		var items=new ArrayList<ItemDto>();
+
+		this.mockMvc.perform(MockMvcRequestBuilders.post("/item")
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.content(this.objectMapper.writeValueAsString(itemDto)))
+				.andExpect(MockMvcResultMatchers.status().isCreated());
+
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/item")
+		.contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("description").value("description"))
+				.andExpect(MockMvcResultMatchers.jsonPath("rateHourBilled").value("1"))
+				.andExpect(MockMvcResultMatchers.jsonPath("ratePrice").value("1.1"))
+				.andExpect(MockMvcResultMatchers.jsonPath("flatPrice").value("1.1"))
+				.andDo(MockMvcRestDocumentation.document("Get-Item", PayloadDocumentation.requestFields(
 						PayloadDocumentation.fieldWithPath("description").description("Item description"),
 						PayloadDocumentation.fieldWithPath("rateHourBilled").description("Item quantity"),
 						PayloadDocumentation.fieldWithPath("ratePrice").description("Item rate price"),
