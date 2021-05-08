3f13aea88d03390a6a273365025857294ee64766
varadharajanthiagarajan <varadha.001@gmail.com>
2021-05-08 11:19:15 -0400

    commit for green - post method implemented with empty function

--- src/main/java/com/groot/invoicify/company/CompanyController.java
+++ src/main/java/com/groot/invoicify/company/CompanyController.java
@@ -1,6 +1,9 @@
 package com.groot.invoicify.company;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +13,11 @@ public class CompanyController {
     String getAllCompany()
     {
         return "{}";
+    }
+
+    @PostMapping("company")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addCompany(){
+
     }
 }
--- src/test/java/com/groot/invoicify/companyIT/CompanyControllerIT.java
+++ src/test/java/com/groot/invoicify/companyIT/CompanyControllerIT.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
 
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -32,6 +34,16 @@ public class CompanyControllerIT {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("length()").value(0));
 
+
+    }
+
+    @Test
+    void initialPostTest() throws Exception{
+
+        mockMvc.perform(post("/company")
+                .content("")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isCreated());
 
     }
 
