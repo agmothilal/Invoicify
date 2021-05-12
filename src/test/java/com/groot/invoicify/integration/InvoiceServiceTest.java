812ec00f1b9bb889318a2c0b8d419d7a7094172d
agmothilal <ag.mothilal@gmail.com>
2021-05-12 17:23:36 -0500

    Update invoice method init commit.

--- src/test/java/com/groot/invoicify/integration/InvoiceServiceTest.java
+++ src/test/java/com/groot/invoicify/integration/InvoiceServiceTest.java
@@ -1,7 +1,6 @@
 package com.groot.invoicify.integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.groot.invoicify.dto.CompanyDto;
 import com.groot.invoicify.dto.InvoiceDto;
 import com.groot.invoicify.dto.ItemDto;
 import org.junit.jupiter.api.Test;
@@ -13,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.restdocs.payload.PayloadDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -30,6 +30,13 @@ public class InvoiceServiceTest {
 	@Autowired
 	ObjectMapper objectMapper;
 
+	private ResultActions createInvoice(InvoiceDto invoiceDto) throws Exception {
+		return this.mockMvc.perform(MockMvcRequestBuilders.post("/invoice")
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.content(this.objectMapper.writeValueAsString(invoiceDto)))
+				.andExpect(MockMvcResultMatchers.status().isCreated());
+	}
+
 	@Test
 	public void createInvoiceTest() throws Exception {
 		var itemsDto = Arrays.asList(
@@ -37,10 +44,7 @@ public class InvoiceServiceTest {
 		);
 		var invoiceDto = new InvoiceDto("Test", "test", false, itemsDto);
 
-		this.mockMvc.perform(MockMvcRequestBuilders.post("/invoice")
-				.contentType(MediaType.APPLICATION_JSON_VALUE)
-				.content(this.objectMapper.writeValueAsString(invoiceDto)))
-				.andExpect(MockMvcResultMatchers.status().isCreated())
+		createInvoice(invoiceDto)
 				.andExpect(MockMvcResultMatchers.jsonPath("$").isNumber())
 				.andDo(MockMvcRestDocumentation.document("Post-Invoice", PayloadDocumentation.requestFields(
 						PayloadDocumentation.fieldWithPath("companyName").description("Name of company on invoice."),
@@ -48,6 +52,22 @@ public class InvoiceServiceTest {
 						PayloadDocumentation.fieldWithPath("author").description("Author of invoice."),
 						PayloadDocumentation.fieldWithPath("paid").description("If company paid the invoice."),
 						PayloadDocumentation.subsectionWithPath("itemsDto[]").description("A list of items in the invoice.")
-						)));
+				)));
+	}
+
+	@Test
+	public void updateInvoiceTest() throws Exception {
+		var itemsDto = Arrays.asList(
+				new ItemDto("Description", 10, 14.50F, 60F)
+		);
+		var invoiceDto = new InvoiceDto("Test", "test", false, itemsDto);
+		var actionResult = createInvoice(invoiceDto);
+		var invoiceId = actionResult.andReturn().getResponse().getContentAsString();
+
+		this.mockMvc.perform(MockMvcRequestBuilders.patch("/invoice")
+				.contentType(MediaType.APPLICATION_JSON)
+				.param("invoiceId", invoiceId)
+				.content(this.objectMapper.writeValueAsString(invoiceDto)))
+				.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 }
